7b120d34d6d87903036fbf159953505cc61607e7
Rafael Gomes <122892963+RafaelGome2@users.noreply.github.com>
2026-02-22 10:25:51 -0300

    chamando funçoes dentro do HTML

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,10 @@
-document.getElementById("texto").innerHTML = "Meu primeiro texto JS!" 
+var pote = "bolachas";
+const pote2 = "bolacha salgada";
+let pote3 = "macarrão";
+//alert(pote3 +" e "+ pote);
+
+document.getElementById("texto").innerHTML=
+ pote;
+ function funçao(){document.getElementById("texto2").innerHTML=
+ pote3;}
+ 
