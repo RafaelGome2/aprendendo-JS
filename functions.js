a52eabdc02d4bce3543c164363614ba4fa7d2f31
Rafael Gomes <122892963+RafaelGome2@users.noreply.github.com>
2026-02-24 21:13:21 -0300

    onMouseOver and Onmouseout

--- functions.js
+++ functions.js
@@ -8,17 +8,28 @@ document.getElementById("texto")
     //funçao cotação do dolar
     function realPdolar(real, cotaçaoDolar){
         return real * cotaçaoDolar;
-            }
-            
+                 
     var real = 50;
     var cotaçaoDolar = 5.18;
     var result = realPdolar(real, cotaçaoDolar );
     alert("o valor em reais= R$"+real
         + " e o valor em dollar = U$"+result.toFixed(2))
+            }
+   
 
         function eventoClik(){
     alert("acionou um evento de click")}
 
     function DbClik(){
         alert("evento de clik duplo")
+            }
+
+    function viraVermelho(){
+        let obj= document.getElementById("div1");
+        obj.style.backgroundColor = "red";
+    }
+
+    function viraAqua(){
+        let obj = document.getElementById("div1");
+        obj.style.backgroundColor = "aqua";
     }
