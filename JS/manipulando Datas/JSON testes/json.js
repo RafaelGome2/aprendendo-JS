f48a4282dc53f4e6eb1fbbc749f0b7655d75044e
Rafael Gomes <122892963+RafaelGome2@users.noreply.github.com>
2026-03-03 15:47:27 -0300

    convertendo objeto para json e vice versa

--- JS/manipulando Datas/JSON testes/json.js
+++ JS/manipulando Datas/JSON testes/json.js
@@ -5,4 +5,20 @@ const carro={
     cor:"branco",
 }
 let objtxt= JSON.stringify(carro);
-document.getElementById("p1").innerHTML=objtxt;
+//document.getElementById("p1").innerHTML=objtxt;
+
+const meuJson={
+ "nome" : "maria",
+ "idade": 25,
+ "email": "maria@gmail.com", 
+ "esta_trabalhando": true,
+ "hobbies":["correr","programar","arduino"]
+}
+console.log(meuJson, "json que criei");
+//converter objeto para json
+const jsontxt = JSON.stringify(meuJson);
+console.log(jsontxt);
+
+//converter json para objeto
+const jsonData = JSON.parse(jsontxt);
+console.log(jsonData);
