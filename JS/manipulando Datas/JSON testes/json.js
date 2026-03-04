c0e4049df1d8b06a408dfcf0707d6268206dc5d1
Rafael Gomes <122892963+RafaelGome2@users.noreply.github.com>
2026-03-04 20:38:01 -0300

    adicionando condiçoes para o usuario
    adicionando condiçoes para o usuário preencher o formulario. Deve ser preenchido por completo, senão lança alert.

--- JS/manipulando Datas/JSON testes/json.js
+++ JS/manipulando Datas/JSON testes/json.js
@@ -25,10 +25,14 @@ const campoCpf= document.querySelector('#cpf');
 form.addEventListener('submit',function(event){
    event.preventDefault();
    const input= {cidade: campoCidade.value, Nome: campoNome.value, Email: campoEmail.value, CPF: campoCpf.value  };
-   
-   const jsontxt= JSON.stringify(input);
+   if(campoNome.value==="" || campoCidade.value==""||campoEmail.value==""||campoCpf.value==""){
+   alert("por favor preencha todos os campos");
+   }
+   else{ const jsontxt= JSON.stringify(input);
    console.log(jsontxt);
-    form.reset ();//para limpar o formulario 
+    form.reset ();//para limpar o formulario }
+   }
+ 
     }
   
    )
