a2f1695b2b9ba81bf4655df4edf0f3048aeccd80
Rafael Gomes <122892963+RafaelGome2@users.noreply.github.com>
2026-03-04 10:56:55 -0300

    pegando varios campos do form para JSON

--- JS/manipulando Datas/JSON testes/json.js
+++ JS/manipulando Datas/JSON testes/json.js
@@ -1,24 +1,37 @@
-const carro={
-    marca:"fiat",
-    modelo: "Uno way 1.4",
-    ano: "2015",
-    cor:"branco",
-}
-let objtxt= JSON.stringify(carro);
-//document.getElementById("p1").innerHTML=objtxt;
 
-const meuJson={
- "nome" : "maria",
- "idade": 25,
- "email": "maria@gmail.com", 
- "esta_trabalhando": true,
- "hobbies":["correr","programar","arduino"]
-}
-console.log(meuJson, "json que criei");
-//converter objeto para json
-const jsontxt = JSON.stringify(meuJson);
-console.log(jsontxt);
+// const meuJson={
+//  "nome" : "maria",
+//  "idade": 25,
+//  "email": "maria@gmail.com", 
+//  "esta_trabalhando": true,
+//  "hobbies":["correr","programar","arduino"]
+// }
+// console.log(meuJson, "json que criei");
+// //converter objeto para json
+// const jsontxt = JSON.stringify(meuJson);
+// console.log(jsontxt);
 
-//converter json para objeto
-const jsonData = JSON.parse(jsontxt);
-console.log(jsonData);
+// //converter json para objeto
+// const jsonData = JSON.parse(jsontxt);
+// console.log(jsonData);
+
+//pegar dados do usuario
+const form = document.querySelector('#form');
+const campoNome = document.querySelector('#nome');
+const campoCidade =document.querySelector('#cidade');
+const campoEmail= document.querySelector('#email');
+const campoCpf= document.querySelector('#cpf');
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    const input =[campoNome.value, campoCidade.value , campoEmail.value, campoCpf.value] ;
+     
+        const jsonTxt = {
+        "nome" : input[0],
+        "cidade" : input[1],
+        "email": input[2],
+        "cpf": input[3]
+    }
+   const jsonObj = JSON.stringify(jsonTxt);
+   console.log(jsonObj);
+})
