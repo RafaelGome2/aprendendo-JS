7c9020a4ff55b314218590a20023c45470da3cd1
Rafael Gomes <122892963+RafaelGome2@users.noreply.github.com>
2026-03-09 15:41:24 -0300

    praticando callbacks e promises

---
+++ testandoApiJavaWorkshopMongo/scrip.js
@@ -0,0 +1,29 @@
+
+// aprendendo sobre Promises :https://www.youtube.com/watch?v=hhDSKhCEzzM&t=1781s
+const fs = require('fs');
+
+console.log(1);
+
+
+
+console.log(2);
+console.log(3);
+
+//fs.readFile('msg.txt',(err, contents)=>{
+//     fs.readFile('msg2.txt',(err2,contents2)=>{
+//          console.log(String(contents));
+//          console.log(String(contents2 ));
+//     })
+    
+// });
+
+
+// const form = document.querySelector('#form');
+// const nomeCampo = document.querySelector('#nome');
+// const emailCampo = document.querySelector('#email');
+
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+// const input ={name: nomeCampo.value, email: emailCampo.value};
+
+// })
